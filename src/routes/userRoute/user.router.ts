5132f216308f7218a78a3482dd5ca24a042259da
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-24 11:45:04 +0300

    fix: fixed authService route and added basic validation

--- src/routes/userRoute/user.router.ts
+++ src/routes/userRoute/user.router.ts
@@ -16,9 +16,14 @@ router.route('/mock').get(async (_req: Request, res: Response, next: NextFunctio
   }
 });
 
-router.route('/authSevice').get(async (_req: Request, res: Response, next: NextFunction) => {
+router.route('/authService').post(async (_req: Request, res: Response, next: NextFunction) => {
   try {
-      res.send(200).json({status: 'ok', token: 'test_token'});
+    if(_req.body.login && _req.body.password) {
+      res.send({ status: 'ok', token: 'test_token' });
+    } else {
+      res.send({ status: 'Error', token: '' });
+    }
+
   } catch (err) {
     next(err);
   }
