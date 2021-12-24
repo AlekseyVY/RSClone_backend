30a49d6225b481c233fd8764a1a1bcec7b4ec7e9
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-24 21:17:06 +0300

    feat: fic=nished auth/login service

--- src/routes/userRoute/user.router.ts
+++ src/routes/userRoute/user.router.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import getAll from './user.service';
 import User, { IUser } from '../../model/User';
 import { createUser, getByLogin } from './user.memory.repository';
+import bcrypt from 'bcrypt';
 
 const uuid = require('uuid').v4;
 
@@ -22,20 +23,22 @@ router.route('/mock').get(async (_req: Request, res: Response, next: NextFunctio
 
 router.route('/authService').post(async (_req: Request, res: Response, next: NextFunction) => {
   try {
-    if (_req.body.login && _req.body.password) {
-      let serviceStatus = await getByLogin(_req.body.login);
-      if (serviceStatus) {
-        res.send({ status: 'ok', token: `${serviceStatus.login}_login` });
-      } else {
-        const user: IUser = new User({
-          id: uuid(), login: _req.body.login, password: _req.body.password
-        });
-        await createUser(user);
-        serviceStatus = await getByLogin(_req.body.login);
-        res.send({ status: 'ok', token: `${serviceStatus}_auth` });
-      }
+    if(!(_req.body.login && _req.body.password)) {
+      res.status(400).send({status: 'Error', token: '', message: 'All inputs required'});
+    }
+    let serviceStatus = await getByLogin(_req.body.login);
+    if(serviceStatus && await bcrypt.compare(_req.body.password, <string>serviceStatus.password)) {
+      res.send({ status: 'logged', userId: serviceStatus.id, token: `${serviceStatus.login}_isAuth` });
     } else {
-      res.send({ status: 'Error', token: '' });
+      const saltRounds = 10;
+      const encryptedPassword = await bcrypt.hash(_req.body.password, saltRounds);
+      const user: IUser = new User({
+        id: uuid(),
+        login: _req.body.login,
+        password: encryptedPassword,
+      });
+      await createUser(user);
+      res.send({ status: 'authenticated', userId: user.id, token: `${user.login}_isAuth` });
     }
   } catch (err) {
     next(err);
