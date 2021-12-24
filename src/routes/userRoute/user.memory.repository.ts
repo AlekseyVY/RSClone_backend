e2e915ad693e80d2a76a6ef15dcb653a02407d30
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-24 21:17:06 +0300

    feat: finished auth/login service

--- src/routes/userRoute/user.memory.repository.ts
+++ src/routes/userRoute/user.memory.repository.ts
@@ -1,6 +1,6 @@
-import { getMongoRepository } from 'typeorm';
+import {getMongoRepository} from 'typeorm';
 import User from '../../entity/user';
-import { IUser } from '../../model/User';
+import {IUser} from '../../model/User';
 
 export const createUser = async (user: IUser) => {
   const userRepository = getMongoRepository(User);
@@ -9,7 +9,5 @@ export const createUser = async (user: IUser) => {
 
 export const getByLogin = async (userLogin: string): Promise<User | undefined> => {
   const userRepository = getMongoRepository(User);
-  const data = userRepository.findOne({ login: userLogin });
-  if (!data) return undefined;
-  return data;
+  return userRepository.findOne({login: userLogin});
 };
