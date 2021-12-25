fcf9d2c84b5a155c0c3db76c67600c40304c83fc
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-25 13:13:48 +0300

    fix: heroku deploy hotfix

--- src/routes/userRoute/user.router.ts
+++ src/routes/userRoute/user.router.ts
@@ -1,11 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import getAll from './user.service';
 import User, { IUser } from '../../model/User';
 import { createUser, getByLogin } from './user.memory.repository';
-import bcrypt from 'bcrypt';
 
 const uuid = require('uuid').v4;
-
 const router = require('express').Router();
 
 router.route('/mock').get(async (_req: Request, res: Response, next: NextFunction) => {
@@ -21,24 +20,41 @@ router.route('/mock').get(async (_req: Request, res: Response, next: NextFunctio
   }
 });
 
-router.route('/authService').post(async (_req: Request, res: Response, next: NextFunction) => {
+router.route('/authService').post(async (req: Request, res: Response, next: NextFunction) => {
   try {
-    if(!(_req.body.login && _req.body.password)) {
-      res.status(400).send({status: 'Error', token: '', message: 'All inputs required'});
+    if (!(req.body.login && req.body.password)) {
+      res.status(400).send({ status: 'Error', token: '', message: 'All inputs required' });
     }
-    let serviceStatus = await getByLogin(_req.body.login);
-    if(serviceStatus && await bcrypt.compare(_req.body.password, <string>serviceStatus.password)) {
-      res.send({ status: 'logged', userId: serviceStatus.id, token: `${serviceStatus.login}_isAuth` });
-    } else {
+    const serviceStatus = await getByLogin(req.body.login);
+    if (serviceStatus && await bcrypt.compare(req.body.password, <string>serviceStatus.password)) {
+      res.send({
+        status: 'logged',
+        userId: serviceStatus.id,
+        token: `${serviceStatus.login}_isAuth`,
+        message: 'Successfully logged in',
+      });
+    } else if (!serviceStatus) {
       const saltRounds = 10;
-      const encryptedPassword = await bcrypt.hash(_req.body.password, saltRounds);
+      const encryptedPassword = await bcrypt.hash(req.body.password, saltRounds);
       const user: IUser = new User({
         id: uuid(),
-        login: _req.body.login,
+        login: req.body.login,
         password: encryptedPassword,
       });
       await createUser(user);
-      res.send({ status: 'authenticated', userId: user.id, token: `${user.login}_isAuth` });
+      res.send({
+        status: 'authenticated',
+        userId: user.id,
+        token: `${user.login}_isAuth`,
+        message: 'Successfully Authenticated',
+      });
+    } else {
+      res.send({
+        status: 'denied',
+        userId: '',
+        token: '',
+        message: 'Wrong password',
+      });
     }
   } catch (err) {
     next(err);
