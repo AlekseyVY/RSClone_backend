9f2ff516cbd21201f818e69e571bfec8d27bc279
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-25 15:41:12 +0300

    fix: deleted cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoute/user.router';
 import 'reflect-metadata';
@@ -8,7 +7,6 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
