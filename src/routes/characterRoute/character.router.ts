c3e823b8d40aad4a58e66572e2441138e198810f
Aleksey Vasiliyev <alekseyvy@gmail.com>
2021-12-28 16:27:51 +0300

    feat: added character route

--- src/routes/characterRoute/character.router.ts
+++ src/routes/characterRoute/character.router.ts
@@ -3,7 +3,6 @@ import router from '../userRoute/user.router';
 
 router.route('/saveCharacter').post(async (req: Request, res: Response, next: NextFunction) => {
   try {
-    console.log(req.body);
     res.status(200).send({ status: 'Ok' });
   } catch (err) {
     next(err);
