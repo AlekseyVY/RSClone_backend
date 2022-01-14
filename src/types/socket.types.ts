b58123f6317714bbdeabbbf84cea3e888d4eb90e
AlekseyVY <alekseyvy@gmail.com>
2022-01-14 19:59:30 +0300

    feat: added socket entity types

---
+++ src/types/socket.types.ts
@@ -0,0 +1,14 @@
+export interface IPlayer {
+  x: number,
+  y: number,
+  rotation: number,
+  playerId: string,
+  firing: boolean
+}
+
+export interface IZombie {
+  x: number,
+  y: number,
+  rotation: number,
+  hp: number
+}
