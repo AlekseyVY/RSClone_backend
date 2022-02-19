21fc2209bd7a4e5cbb306d7195a6644dcb2c3b4a
AlekseyVY <alekseyvy@gmail.com>
2022-02-19 12:27:21 +0300

    fix: fix

--- src/services/issueToken.ts
+++ src/services/issueToken.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const issueToken = (username: string): string => {
   const secret = process.env.SECRET;
   const token = secret && jwt.sign({ username }, secret);
-  return token ? token : 'NOT_WORKING'
+  return token || 'NOT_WORKING';
 };
 
 export default issueToken;
